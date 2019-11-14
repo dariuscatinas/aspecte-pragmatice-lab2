3fa22a017157cd79dbc39da5fb2b125517797036
Darius <dariuscatimas@gamil.com>
2019-11-14 13:56:27 +0200

    modified source file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.net.URL;
 
 public class Main {
-    private static final String versionHash = "a813ebfd97";
+    private static final String versionHash = "d1516dec12";
     public static void main(String[] args) {
-        System.out.println(versionHash);
+        System.out.println("NEW " + versionHash);
     }
 
 }
