d1516dec12ee5b53d7be8e5ea3e3bac3a058d208
Darius <dariuscatimas@gamil.com>
2019-11-14 13:53:10 +0200

    finalLab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,39 +2,9 @@ import java.io.*;
 import java.net.URL;
 
 public class Main {
+    private static final String versionHash = "a813ebfd97";
     public static void main(String[] args) {
-        System.out.println(readBuildVersion());
+        System.out.println(versionHash);
     }
-    private static int readBuildVersion(){
-        int buildNo = -1;
-        Main mainInstance = new Main();
-        File file = mainInstance.getFileFromResources("version.txt");
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            String line = reader.readLine();
-            buildNo = Integer.parseInt(line);
-        }
-        catch(FileNotFoundException ex){
-            System.out.println("File does not exist");
-        }
-        catch (IOException ex){
-            System.out.println("Input exception");
-        }
-        catch(NumberFormatException ex){
-            System.out.println("Cannot cast build no to file");
-        }
-        return buildNo;
-    }
-    private File getFileFromResources(String fileName) {
 
-        ClassLoader classLoader = getClass().getClassLoader();
-
-        URL resource = classLoader.getResource(fileName);
-        if (resource == null) {
-            throw new IllegalArgumentException("file is not found!");
-        } else {
-            return new File(resource.getFile());
-        }
-
-    }
 }
